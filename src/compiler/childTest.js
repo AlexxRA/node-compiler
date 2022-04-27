5d2d4d398c77e12cab5729c744a61c75cf84a8d7
Angel Rosales <angel.rosales@geodis.com>
2022-04-26 21:40:15 -0500

    testing purposes

---
+++ src/compiler/childTest.js
@@ -0,0 +1,79 @@
+const { exec } = require('child_process');
+const fs = require('fs/promises');
+
+var targetSource;
+var targetExe;
+var targetInput;
+var fileName;
+
+process.on('message', (message) => {
+    fileName = 'script' + message.id + '_' + new Date().getTime().toString();
+    targetSource = __dirname+'/scripts/codeFiles/'+fileName+'.cpp';
+    targetExe = __dirname+'/scripts/exe/'+fileName;
+    targetInput = __dirname+'/scripts/inputs/' + fileName + '.txt';
+
+    fs.writeFile(targetSource, message.script, (errorSource) => {
+        if (errorSource){
+            console.log(`errorSource: ${errorSource}`);
+            process.send({error: errorSource, success:false, id:message.id});
+            return;
+        }
+        console.log(`cpp created`);
+        fs.writeFile(targetInput, message.inputs, (errorInput) => {
+            if (errorInput){
+                console.log(`errorInput: ${errorInput}`);
+                process.send({error: errorInput, success:false, id:message.id});
+                return;
+            }
+            console.log(`txt created`);
+            exec(`g++ ${targetSource} -o ${targetExe}`, (error, stdout, stderr) => {
+                if (error) {
+                    console.log(`error: ${error.message}`);
+                    process.send({error: error.message, success:false, id:message.id});
+                    return;
+                }
+            
+                if (stderr) {
+                    console.log(`stderr: ${stderr}`);
+                    process.send({error: stderr, success:false, id:message.id});
+                    return;
+                }
+            
+                console.log(`stdout: ${stdout}`);
+            
+                exec(`cd ${__dirname}/scripts/exe;  ./${fileName} < ${targetInput}`, (errorExe, stdoutExe, stderrExe) => {
+                    if (errorExe) {
+                        console.log(`errorExe: ${errorExe.message}`);
+                        process.send({error: errorExe.message, success:false, id:message.id});
+                        return;
+                    }
+                
+                    if (stderrExe) {
+                        console.log(`stderrExe: ${stderrExe}`);
+                        process.send({error: stderrExe, success:false, id:message.id});
+                        return;
+                    }
+                
+                    //********exe result*********
+                    console.log(`stdoutExe: ${stdoutExe}`);
+                    process.send({result: stdoutExe, success:true, id:message.id});
+                    //********exe result*********
+
+                    exec(`rm ${targetSource}; rm ${targetExe}; rm ${targetInput}`, (errorRm, stdoutRm, stderrRm) => {
+                        if (errorRm) {
+                            console.log(`errorRm: ${errorRm.message}`);
+                            return;
+                        }
+                    
+                        if (stderrRm) {
+                            console.log(`stderrRm: ${stderrRm}`);
+                            return;
+                        }
+                    
+                        console.log(`stdoutRm: ${stdoutRm}`);
+                    });
+                });
+            });
+        });
+    });
+});
--- src/compiler/compiler.js
+++ src/compiler/compiler.js
@@ -3,9 +3,14 @@ const router = express.Router()
 var cp = require('child_process');
 var Sync = require('sync');
 
+const { exec } = require('child_process');
+const fs = require('fs');
+
+
 router.use(logger)
 
 var child = cp.fork(__dirname + '/child.js');
+// var child = cp.fork(__dirname + '/childTest.js');
 var taskId = 0;
 var tasks = {};
 var maxQueue = 10;
@@ -19,7 +24,7 @@ router.get("/", (req, res) => {
 })
 
 //POST
-router.post('/compile', (req, res) => {
+router.post('/', (req, res) => {
     res.header('Access-Control-Allow-Origin', '*');
 
     var script = req.body.script;
@@ -30,6 +35,42 @@ router.post('/compile', (req, res) => {
     });
 	
 });
+
+//POST TEST
+router.post('/test', async (req, res) => {
+    var code = req.body.code;
+
+    var fileName = 'script' + '_' + new Date().getTime().toString();
+    var targetSource = __dirname+'/scripts/codeFiles/'+fileName+'.cpp';
+    var targetExe = __dirname+'/scripts/exe/'+fileName;
+
+
+    fs.writeFile(targetSource, code, (errorSource) => {
+        if (errorSource){
+            console.log(`errorSource: ${errorSource}`);
+            res.status(400).json({
+                message: errorSource,
+                success: false
+            });
+        }else{
+            exec(`g++ ${targetSource} -o ${targetExe}`, (error, stdout, stderr) => {
+                if (error) {
+                    console.log(`error: ${error.message}`);
+                    process.send({error: error.message, success:false, id:message.id});
+                    return;
+                }
+            
+                if (stderr) {
+                    console.log(`stderr: ${stderr}`);
+                    process.send({error: stderr, success:false, id:message.id});
+                    return;
+                }
+                console.log(`stdout: ${stdout}`);
+            });
+        }
+    });
+    console.log("END!!!");
+})
 //********API ENDPOINTS **********/
 
 
